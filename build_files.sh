9271db63563ba6c7cd7037a05eb4776509475446
rohitrajput-42 <rohit1471997@gmail.com>
2024-12-30 22:56:19 +0530

    updated build file

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
-echo " BUILD START"
-python3.10 -m  pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
-echo " BUILD END"
+#!/bin/bash
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run collectstatic to gather all static files
+python manage.py collectstatic --noinput
+
+# Move static files to the correct directory for Vercel
+mkdir -p staticfiles_build
+cp -r staticfiles/* staticfiles_build/
