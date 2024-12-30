090a1ad15324d9c60597ac4e5e067be9a8fc1e5c
rohitrajput-42 <rohit1471997@gmail.com>
2024-12-30 22:54:02 +0530

    updated urls for static files

--- monetize/urls.py
+++ monetize/urls.py
@@ -9,3 +9,6 @@ urlpatterns = [
     path("", include("home.urls")),
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
