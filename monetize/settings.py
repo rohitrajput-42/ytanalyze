41d50cb3cf40f629626bbeb3a3b185b610826bde
rohitrajput-42 <rohit1471997@gmail.com>
2024-12-31 09:33:00 +0530

    try-101

--- monetize/settings.py
+++ monetize/settings.py
@@ -117,10 +117,8 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
+STATIC_URL = '/ui/'
 
 STATIC_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'ui')
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
