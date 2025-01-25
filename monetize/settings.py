11ec7960d41af2ece3d30af5cf6365eac86915c0
rohitrajput-42 <rohit1471997@gmail.com>
2025-01-25 22:13:45 +0530

    updated settings for vercel and ui

--- monetize/settings.py
+++ monetize/settings.py
@@ -109,7 +109,5 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = 'ui/'
-
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'ui')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'ui'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'ui')
