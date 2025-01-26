bfc2fab53ffa8be1d8b6bf6a3a43c091167de807
rohitrajput-42 <rohit1471997@gmail.com>
2025-01-26 12:57:44 +0530

    cookie from browser

--- home/views.py
+++ home/views.py
@@ -111,7 +111,7 @@ def thumb_fetch(request):
         options = {
             'quiet': True,
             'extract_flat': True,
-            'cookiefile': 'path/to/cookies.txt',
+            'cookies_from_browser': True,
             'playlistend': 1
         }
         
