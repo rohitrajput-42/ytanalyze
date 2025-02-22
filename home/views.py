1ef485ff0225c2b2b2d1f6091e43eb2e58a77870
rohitrajput-42 <rohit1471997@gmail.com>
2025-02-22 14:48:06 +0530

    updated client header for monetization check

--- home/views.py
+++ home/views.py
@@ -61,7 +61,7 @@ def monetize(request):
             video_url = f"https://www.youtube.com/watch?v={video['id']}"
 
             headers = {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36'
             }
             response = requests.get(video_url, headers=headers)
             response.raise_for_status()
